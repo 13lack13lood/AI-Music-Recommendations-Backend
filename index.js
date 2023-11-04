e5c2ed14519ea1ec463cb956628b21ff57ddf676
13lack13lood <harryzhu155@gmail.com>
2023-11-04 11:45:39 -0400

    can receive artist data

--- index.js
+++ index.js
@@ -15,38 +15,63 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 const configuration = new Configuration({
-	apiKey: process.env.OPEN_AI_KEY,
+    apiKey: process.env.OPEN_AI_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-app.post("/get-recommendation", async (req, res) => {
-	try {
-		const { prompt } = req.body;
+app.post("/get-recommendation-track", async (req, res) => {
+    try {
+        const { prompt } = req.body;
 
-		const content =
-			"give me 15 new songs giving the name and artist given that I like the following songs and send the result as a json object in the format:\nsong1: {\nname: ...\nartist: ...\n}\n" +
-			prompt;
-		
-		console.log(content);
+        const content = "give me 15 new songs giving the name and artist given that I like the following songs and send the result as a json object in the format:\nsong1: {\nname: ...\nartist: ...\n}\n" + prompt;
 
-		const response = await openai.createChatCompletion({
-			model: "gpt-3.5-turbo",
-			messages: [{ role: "user", content: content }],
-		});
+        console.log(content);
 
-		console.log(response.data.choices[0].message.content);
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: content }],
+        });
 
-		return res.status(200).json({
-			message: "Working",
-		 data: response.data.choices[0].message.content,
-		});
-	} catch (error) {
-		return res.status(400).json({
-			success: false,
-			error: error.response ? error.response.data : "There was an issue on the server",
-		});
-	}
+        console.log(response.data.choices[0].message.content);
+
+        return res.status(200).json({
+            message: "Working",
+            data: response.data.choices[0].message.content,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error: error.response ? error.response.data : "There was an issue on the server",
+        });
+    }
+});
+
+app.post("/get-recommendation-artist-artist", async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const content = "give me 12 new artists given that I like the following artists and send the result as a json object in the format:\nartist1: {\nname: ...\n}\n" + prompt;
+
+        console.log(content);
+
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: content }],
+        });
+
+        console.log(response.data.choices[0].message.content);
+
+        return res.status(200).json({
+            message: "Working",
+            data: response.data.choices[0].message.content,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error: error.response ? error.response.data : "There was an issue on the server",
+        });
+    }
 });
 
 const port = process.env.PORT || 5000;
